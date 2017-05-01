380245260268f17df309199cbd431496ffa3baa6
gongbinglai <gongbinglai@jd.com>
2017-05-01 09:56:10 +0800

    解释器模式：对一种语法树进行解析，包含终结表达式、非终结表达式、环境上下文

---
+++ src/main/java/com/jd/dp/interpret/Variable.java
@@ -0,0 +1,31 @@
+package com.jd.dp.interpret;
+
+public class Variable extends Expression {
+
+	private String name;
+
+	public Variable(String name) {
+		this.name = name;
+	}
+
+	public boolean equals(Object obj) {
+
+		if (obj != null && obj instanceof Variable) {
+			return this.name.equals(((Variable) obj).name);
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+
+	public String toString() {
+		return name;
+	}
+
+	public boolean interpret(Context ctx) {
+		return ctx.lookup(this);
+	}
+
+}
